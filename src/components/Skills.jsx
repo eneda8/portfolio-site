8a4f62081ff22150438577457a416ab49d4edbef
Eneda Xhambazi <enedaxhambazi@gmail.com>
2023-04-08 00:54:53 -0400

    simplified skill component markup and improved responsiveness

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,25 +5,17 @@ export default function Skills(props) {
   const skills = props.skills.icons.map(function (skill) {  
 
     return (
-      <li className="list-inline-item mx-lg-3 " key={nanoid()}>
-        <div id={skill.name.slice(0,2)} className="text-center skills-tile">
-           <i className={skill.class}></i> 
-          <p className="text-center">{skill.name}</p>
-        </div>
-      </li>
+      <div id={skill.name.slice(0,2)} className="text-center skills-tile" key={nanoid()}>
+        <i className={skill.class}></i> 
+        <p className="text-center">{skill.name}</p>
+      </div>
     );
   });
 
   return (
     <section id="skills">
-      <div className="col-md-12">
-        <div className="col-md-12">
-          <h1 className="section-title text-white">Skills</h1>
-        </div>
-        <div className="col-md-12 text-center">
-          <ul className="list-inline mx-auto skill-icon">{skills}</ul>
-        </div>
-      </div>
+      <h1 className="section-title text-white">Skills</h1>
+      <div className="skills-container">{skills}</div>
     </section>
   );
 };
